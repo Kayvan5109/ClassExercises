bfa9fd845f1e0fde36857c71cd21410ad73dcf0d
Kayvan-Khoobehi <khoobehi.kayvan@gmail.com>
2023-01-19 14:09:53 -0600

    Finish ex3

--- Week1/ex3.py
+++ Week1/ex3.py
@@ -1,2 +1,10 @@
 #Write a python function to compute the sum of all odd numbers in a range. Your function will take two numbers as input (that specifies a range) and return a number.
+def sum_odd(x, y):
+    sum = 0 
+    for i in range(x, y+1):
+        if i%2 !=0:
+            sum += i 
+    return sum 
+
+print(sum_odd(1, 10))
 
