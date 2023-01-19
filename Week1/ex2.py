172cadda6fa3ada9faf664d11a6a3046c6b470a4
Kayvan-Khoobehi <khoobehi.kayvan@gmail.com>
2023-01-19 12:11:00 -0600

    Added ex2 and ex3

---
+++ Week1/ex2.py
@@ -0,0 +1,3 @@
+#Write a python function to count the number of characters in a text where text is the input to the function. Your function will return a number.
+
+len("Hello World")
---
+++ Week1/ex3.py
@@ -0,0 +1,2 @@
+#Write a python function to compute the sum of all odd numbers in a range. Your function will take two numbers as input (that specifies a range) and return a number.
+
