f96ed1bd642e81a503071f437f45c398cccbdefe
Kayvan-Khoobehi <khoobehi.kayvan@gmail.com>
2023-01-19 12:16:44 -0600

    update ex2.py

--- Week1/ex2.py
+++ Week1/ex2.py
@@ -1,3 +1,5 @@
 #Write a python function to count the number of characters in a text where text is the input to the function. Your function will return a number.
+def my_function(textinput): 
+    print(len(textinput))
 
-len("Hello World")
+my_function("Hello")
