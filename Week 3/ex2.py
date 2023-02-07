eae7a505a343540758e5a715e0a35308377c029f
Kayvan-Khoobehi <khoobehi.kayvan@gmail.com>
2023-02-07 10:56:11 -0600

    Updated Exercise 2

--- Week 3/ex2.py
+++ Week 3/ex2.py
@@ -1,7 +1,7 @@
 import requests 
 import json 
 
-API_KEY = "SGCO2WYJFACAICG2 "
-url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=TSLA&apikey=YOUR_API_KEY"
+API_KEY = "SGCO2WYJFACAICG2"
+url = "https://www.alphavantage.co/query?function=OVERVIEW&symbol=TSLA&apikey=SGCO2WYJFACAICG2"
 data = requests.get(url)
 print(json.dumps(data.json(), indent = 4))
